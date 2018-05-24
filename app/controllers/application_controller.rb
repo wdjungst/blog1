1c80f677d1b7b7e814acbe4bcf8c6f3eff699172
David Jungst <djungst@gmail.com>
2018-05-24 12:04:32 -0600

    page creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def foo
+    #TODO add something later
+    #FIXME Sarah broke this ill get back to it
+    #OPTIMIZE Dan made this super slow
+  end
 end
